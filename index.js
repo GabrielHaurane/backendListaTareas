fe68f7410b730fde5b255d5f47f8e1b7602aeebf
Gabriel Haurane <gabrielhaurane@gmail.com>
2024-10-04 16:48:08 -0300

    agregue los middlewares y el index.html

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from "express";
-
+import morgan from 'morgan'
+import cors from 'cors'
 const app = express();
-
+// 1
 app.set("port", process.env.PORT || 4000)
 app.listen(app.get("port"), ()=>{
-    console.info('estoy escuchando el puerto'+ app.get('port') )
+    console.info('estoy escuchando el puerto '+ app.get('port') )
+})
+// 2
+app.use(cors()) // permite conexiones remotas
+app.use(morgan('dev')) // da info extra en al terminal (cuando usamos el postman da el get 200)
+app.use(express.json()) // interpreta los datos del body en json
+app.use(express.urlencoded({extended:true})) // interpreta datos enviados en formularios
+
+app.get('/tareas', (req,res)=>{
+res.send('desde bd')
 })
